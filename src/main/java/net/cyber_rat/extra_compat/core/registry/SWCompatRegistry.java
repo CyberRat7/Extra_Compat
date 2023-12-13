b8aaacf4206484766bec4866e03a4e5a167f1ede
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-12-13 19:10:52 +0100

    Changes

--- src/main/java/net/cyber_rat/extra_compat/core/registry/SWCompatRegistry.java
+++ src/main/java/net/cyber_rat/extra_compat/core/registry/SWCompatRegistry.java
@@ -2,10 +2,7 @@ package net.cyber_rat.extra_compat.core.registry;
 
 import net.cyber_rat.extra_compat.core.registry.object.ExtraCompatItems;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.*;
 import net.minecraftforge.registries.RegistryObject;
 
 import static net.cyber_rat.extra_compat.core.registry.object.ExtraCompatCreativeModeTabs.CREATIVE_MODE_TABS;
@@ -17,7 +14,7 @@ public class SWCompatRegistry {
             () -> new SwordItem(ExtraCompatTiers.EXTRA_COMPAT_REINFORCED_GOLD, 8, -2.9F, (new Item.Properties().stacksTo(1))));
 
     public static final RegistryObject<Item> REINFORCED_GREAT_AXE = ExtraCompatItems.ITEMS.register("reinforced_gold_great_axe",
-            () -> new SwordItem(ExtraCompatTiers.EXTRA_COMPAT_REINFORCED_GOLD, 10, -3.2F, (new Item.Properties().stacksTo(1))));
+            () -> new AxeItem(ExtraCompatTiers.EXTRA_COMPAT_REINFORCED_GOLD, 10, -3.2F, (new Item.Properties().stacksTo(1))));
 
     public static final RegistryObject<CreativeModeTab> SW_COMPAT_TAB =
             CREATIVE_MODE_TABS.register("sw_compat_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(SWCompatRegistry.
